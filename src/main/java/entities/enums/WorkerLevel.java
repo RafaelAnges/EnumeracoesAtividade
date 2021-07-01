9dcf5caf6d8118867546f7fbd65b602b06a019fd
Rafael Santos <rafael_16001@hotmail.com>
2021-07-01 10:16:58 -0300

    modifiquei o WorkerLevel

--- src/main/java/entities/enums/WorkerLevel.java
+++ src/main/java/entities/enums/WorkerLevel.java
@@ -1,9 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package entities.enums;
 
+/**
+ *
+ * @author rafae
+ */
 public enum WorkerLevel {
-	
-	JUNIOR,
+        JUNIOR,
 	MID_LEVEL,
 	SENIOR;
-
 }
